4b3272d03f7987c6b541a3d8dcb6c15316ade081
hituhin <hituhin09@gmail.com>
2022-07-31 17:51:58 +0600

    Initialized poritfolio app development

---
+++ components/main/landingPage/LandignPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LandignPage = () => {
+  return (
+    <h1>This is the landing Page</h1>
+  )
+};
+
+export default LandignPage;
---
+++ pages/main/landing-page/index.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import LandingComponent from '../../../components/main/landingPage/LandignPage'
+
+const index = () => {
+    return (
+        <Fragment>
+            <LandingComponent/>
+        </Fragment>
+    )
+}
+
+export default index
